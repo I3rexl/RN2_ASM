47a33018dfdc1e53859fe10f05c7134e898576db
I3rexl <I3rexl@MacBook-of-I3rexl.local>
2025-03-27 09:51:06 +0700

    ASM

---
+++ app/Login.tsx
@@ -0,0 +1,11 @@
+import { View, Text, TextInput, StyleSheet, Image, TouchableOpacity } from "react-native";
+
+const login= () =>{
+    return(
+        <View>
+            <Text></Text>
+        </View>
+    )
+}
+
+export default login;
